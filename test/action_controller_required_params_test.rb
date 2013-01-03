8fc75847c2fadd28227430d806ace8e8c48668ad
Rob Nichols <git@nicholshayes.co.uk>
2013-01-03 10:07:39 +0000

    Reenables tests disabled during debugging

--- test/action_controller_required_params_test.rb
+++ test/action_controller_required_params_test.rb
@@ -21,33 +21,32 @@ class ActionControllerRequiredParamsTest < ActionController::TestCase
   end
   
   test "missing second lavel required parameters will raise exception" do
-
     post :create, { :book => { :title => "Mjallo!" } }
     assert_response :bad_request
   end
   
-#  test "missing required parameters will raise exception when require chained" do
-#    post :create_with_chained_require, { :magazine => { :name => "Mjallo!" }, :hat => 'bowler' }
-#    assert_response :bad_request
-#
-#    post :create_with_chained_require, { :book => { :title => "Mjallo!" }, :hat => 'bowler' }
-#    assert_response :bad_request
-#    
-#    post :create_with_chained_require, { :book => { :name => "Mjallo!" } }
-#    assert_response :bad_request
-#  end
-#
-#  test "required parameters that are present will not raise" do
-#    post :create, { :book => { :name => "Mjallo!" } }
-#    assert_response :ok
-#    
-#    post :create_with_chained_require, { :book => { :name => "Mjallo!" }, :hat => 'bowler' }
-#    assert_response :ok
-#  end
-#
-#  test "missing parameters will be mentioned in the return" do
-#    post :create, { :magazine => { :name => "Mjallo!" } }
-#    assert_match "Required parameter missing: 'book'", response.body
-#  end
+  test "missing required parameters will raise exception when require chained" do
+    post :create_with_chained_require, { :magazine => { :name => "Mjallo!" }, :hat => 'bowler' }
+    assert_response :bad_request
+
+    post :create_with_chained_require, { :book => { :title => "Mjallo!" }, :hat => 'bowler' }
+    assert_response :bad_request
+    
+    post :create_with_chained_require, { :book => { :name => "Mjallo!" } }
+    assert_response :bad_request
+  end
+
+  test "required parameters that are present will not raise" do
+    post :create, { :book => { :name => "Mjallo!" } }
+    assert_response :ok
+    
+    post :create_with_chained_require, { :book => { :name => "Mjallo!" }, :hat => 'bowler' }
+    assert_response :ok
+  end
+
+  test "missing parameters will be mentioned in the return" do
+    post :create, { :magazine => { :name => "Mjallo!" } }
+    assert_match "Required parameter missing: 'book'", response.body
+  end
   
 end
